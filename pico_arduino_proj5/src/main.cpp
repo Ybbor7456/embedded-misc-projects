32b8e971f16b6b6247e112d37bd5fcc605318842
Robby <robby7456@gmail.com>
2025-10-06 19:22:39 -0400

    updated and added menu functionalty

--- pico_arduino_proj5/src/main.cpp
+++ pico_arduino_proj5/src/main.cpp
@@ -6,6 +6,11 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define DRAW_H 20
+#define DRAW_W 88
+
+static const char* ITEMS[] = {"Tuner", "ADC Bar", "About"};
+static const int ITEM_COUNT = sizeof(ITEMS)/sizeof(ITEMS[0]);
 #define OLED_RESET -1           // No reset pin on many I2C modules; share MCU reset
 #define OLED_ADDR 0x3C
 
@@ -15,10 +20,52 @@ const int VREF = 5;
 const int ADCMAX = 1023; //may be different on Uno 
 constexpr int POT_PIN = A0; 
 constexpr int BTN_PIN = 2;
-uint8_t page = 0; 
+ 
+enum class Page: int{Menu, Tuner, Settings, About};
+Page activePage = Page::Menu;      
+uint8_t menuIndex = 0; 
 
+int adcToMenuIndex(int adc) {
+  // 3 equal buckets (0..1023) -> 0,1,2 with hysteresis
+  static uint8_t sel = 0;
+  static int lastChange = 0;
+  int div = map(adc, 0, 1023, 0, (int)ITEM_COUNT); // range of 0 to 3
 
-void drawScreenTwo(){
+  const int HYST = 30; // ADC counts (~3%), prevents from drastic/shaky changes in OELD
+  if (div != sel) {
+    if (abs(adc - lastChange) > HYST) { sel = div; lastChange = adc; }
+  }
+  return sel;
+}
+
+void drawMenu(int selected){
+    display.clearDisplay();
+    display.setTextSize(1);
+    //int w = map(raw, 0, ADCMAX, 0, SCREEN_WIDTH);     // remaps v to range of 0-1023 to the width of 0 to screen width
+    //w = constrain(w,0,SCREEN_WIDTH);
+
+    for(int i = 0; i < ITEM_COUNT; i++){
+        display.drawRect(DRAW_H, 20*i, DRAW_W, 16, SSD1306_WHITE);               // (20,0) -> (20,20) = (+0, +20)
+        display.setCursor(45, 5 +(20*i));             // (x,y) -> (0, 5) -> (+0, +20)
+        display.print(ITEMS[i]);
+
+        if (i == selected) {
+        display.fillRect(DRAW_H, 20*i, DRAW_W, 16, SSD1306_WHITE);
+  }
+    }
+
+   display.display(); 
+}
+
+void drawTuner(){
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(8, 8);
+    display.print(F("Tuner"));
+    display.display();
+    }
+
+void drawBarScreen(){
   int raw = analogRead(A0); 
   int w = map(raw, 0, ADCMAX, 0, SCREEN_WIDTH);     // remaps v to range of 0-1023 to the width of 0 to screen width
   w = constrain(w,0,SCREEN_WIDTH);
@@ -33,34 +80,21 @@ void drawScreenTwo(){
   display.display();
 }
 
-void drawScreenOne(){
+void drawSettings(){
     display.clearDisplay();
-    int raw = analogRead(A0); 
-    //int w = map(raw, 0, ADCMAX, 0, SCREEN_WIDTH);     // remaps v to range of 0-1023 to the width of 0 to screen width
-    //w = constrain(w,0,SCREEN_WIDTH);
-
-    if(raw >= 250 && raw < 450){
-    display.fillRect(20, 0, 88, 16, SSD1306_WHITE);
-    }
-    else if(raw >= 450 && raw < 650){
-    display.fillRect(20, 22, 88, 16, SSD1306_WHITE);
-    }
-    else if(raw >= 650 && raw < 850){ 
-    display.fillRect(20, 44, 88, 16, SSD1306_WHITE);
-    }
-   
-    // draws 4 rectangles for menu with text
-    display.drawRect(20, 0, SCREEN_WIDTH - 40, 16, SSD1306_WHITE);
-    display.setCursor(45,5);
-    display.print("Tuner"); 
-    display.drawRect(20, 20, SCREEN_WIDTH -40, 16, SSD1306_WHITE);
-    display.setCursor(45,25);
-    display.print("Settings");
-    display.drawRect(20, 40, SCREEN_WIDTH -40, 16, SSD1306_WHITE);
-    display.setCursor(45,45);
-    display.print("About"); 
+    display.setTextSize(2);
+    display.setCursor(8, 8);
+    display.print(F("Settings"));
     display.display();
-    }
+}
+
+void drawAbout(){
+    display.clearDisplay();
+    display.setTextSize(3);
+    display.setCursor(20,20);
+    display.print("About");
+    display.display();
+}
 
 bool readButtonPressed() {
   static uint32_t lastChange = 0;
@@ -80,7 +114,7 @@ void setup() {
   Wire.begin();              // SDA and SCLK
   delay(100);
   
-  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
     Serial.println(F("SSD1306 init failed"));
     while (1) {}
   }
@@ -90,28 +124,42 @@ void setup() {
   display.setCursor(0, 0);
   display.println(F("Hello OLED"));
   display.setTextSize(1);
-  display.println(F("Arduino UNO + I2C"));
+  display.println(F("Arduino UNO + I^2C"));
   display.display();
   delay(1200);
-
-/*
-  display.clearDisplay();
-  display.drawRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SSD1306_WHITE);
-  display.fillCircle(100, 16, 10, SSD1306_WHITE);
-  display.drawLine(0, 32, 127, 32, SSD1306_WHITE);
-  display.setCursor(6, 10);
-  display.setTextSize(1);
-  display.println(F("SSD1306 demo"));
-  display.display();
-*/
 }
 
 void loop() {
-    if (readButtonPressed()) {
-    page ^= 1; // toggle 0<->1 xor
-  } 
-    if (page == 0) drawScreenOne();
-        else   drawScreenTwo();
+    int raw = analogRead(A0);
+    switch(activePage){
 
+    case Page::Menu: {
+      // pot only moves HIGHLIGHT, never changes the page:
+      menuIndex = adcToMenuIndex(raw);
+      drawMenu(menuIndex);
+      if (readButtonPressed()) {
+        activePage = (menuIndex == 0) ? Page::Tuner
+                   : (menuIndex == 1) ? Page::Settings
+                                      : Page::About;
+      }
+    } break;
+        
+        case Page:: Tuner:{
+            drawTuner();
+            if (readButtonPressed()) activePage = Page::Menu; 
+        } break;
+
+        case Page::Settings:{
+            drawBarScreen();
+            if(readButtonPressed()) activePage = Page::Menu; 
+
+        } break;
+
+        case Page::About:{
+            drawAbout();
+            if(readButtonPressed()) activePage = Page::Menu; 
+        } break;
+    
+    }
   delay(50);
 }
